2936132fc89e36ee2bcd7b390b4b99c54773fec8
Gabriel Maso <maso.gabriel97@gmail.com>
2025-03-02 22:06:43 -0300

    feat: Se agrega pagina Tareas, su formulario y el componente Navigation

---
+++ frontend/src/components/Navigation.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+function Navigation() {
+  return (
+    <div>
+        <Link to='/tasks'>
+            <h1>Task App</h1>
+        </Link>
+        <Link to="/task-create">Create Task</Link>
+    </div>
+  )
+}
+
+export default Navigation
