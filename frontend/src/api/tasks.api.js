6a3b8e2089b9f0296cf245ec04888dc90a1819d9
Gabriel Maso <maso.gabriel97@gmail.com>
2025-03-06 21:26:39 -0300

    feat:se agrega la peticion  axios post, base URL

--- frontend/src/api/tasks.api.js
+++ frontend/src/api/tasks.api.js
@@ -1,5 +1,14 @@
 import axios from 'axios'
 
+const taskApi = axios.create ({
+    baseURL: 'http://127.0.0.1:8000/api/tasks/'
+})
+
 export const getAllTasks = () => {
-    return axios.get('http://127.0.0.1:8000/api/tasks/')
+    //return axios.get('http://127.0.0.1:8000/api/tasks/')
+    return taskApi.get('/')
+}
+
+export const createTask = (task) => {
+    return taskApi.post('/', task)
 }
