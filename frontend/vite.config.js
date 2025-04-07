81e21599f2d7fba6655d8c217f51d6d8a7b1b350
Gabriel Maso <maso.gabriel97@gmail.com>
2025-04-07 20:43:41 -0300

    Agregando Variable de entorno

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,13 +4,4 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000', // Reemplaza con la URL de tu backend
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
